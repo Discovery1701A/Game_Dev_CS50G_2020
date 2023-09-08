63195541ffdd5dd8152176ab89455ca488e17570
Discovery1701A <klettern.rosine0v@icloud.com>
2023-09-08 10:07:37 +0200

    Update main.lua

--- Pong_L1/main.lua
+++ Pong_L1/main.lua
@@ -10,6 +10,8 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    math.randomseed(os.time())
+
     love.window.setTitle('Pong')
 
     smallFont = love.graphics.newFont('EarthrealmBold-Edmg.ttf', 10)
@@ -24,24 +26,52 @@ function love.load()
     player2Score = 0
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 50
+
+
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = 'start'
+
 end
 
 function love.update(dt)
     if love.keyboard.isDown('w') then
-        player1Y = player1Y - PADDLE_SPEED * dt
+        player1Y = math.max(0,player1Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('s') then
-        player1Y = player1Y + PADDLE_SPEED * dt
+        player1Y = math.min(VIRTUAL_HEIGHT-20,player1Y + PADDLE_SPEED * dt)
     end
     if love.keyboard.isDown('up') then
-        player2Y = player2Y - PADDLE_SPEED * dt
+        player2Y = math.max(0,player2Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('down') then
-        player2Y = player2Y + PADDLE_SPEED * dt
+        player2Y = math.min(VIRTUAL_HEIGHT-20,player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
+
+
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        elseif gameState == 'play' then
+            gameState = 'start'
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50)
+        end
     end
 end
 
@@ -51,6 +81,7 @@ function love.draw()
     --love.graphics.clear(40, 45, 52, 255)
     love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 8 - 6, VIRTUAL_WIDTH, 'center')
+
     love.graphics.setFont(soreFont)
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
@@ -63,7 +94,7 @@ function love.draw()
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     --ball
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
+    love.graphics.rectangle('fill',ballX,ballY,4,4)
 
     push:apply('end')
 end
