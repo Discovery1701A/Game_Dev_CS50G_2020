11bbaab099a886f7f67c92eb2c282188aa4c611b
Discovery1701A <klettern.rosine0v@icloud.com>
2023-09-10 14:02:32 +0200

    countdown

--- Voegelchen/states/TitleScreenState.lua
+++ Voegelchen/states/TitleScreenState.lua
@@ -2,7 +2,7 @@ TitleScreenState = Class{__includes = BaseState}
 
 function TitleScreenState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        gStateMachine:change('play')
+        gStateMachine:change('countdown')
     end
 end
 
